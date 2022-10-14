1c1b533276ba972ae00005a05246a294f75f02df
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2022-10-14 22:46:57 +0000

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -1,6 +1,6 @@
 
-sudo dpkg -i PharosControl-2.0.0-1.ub16.noarch.deb
-sudo apt install -yf
-sudo /etc/init.d/pharoscontrol start
-sudo /etc/init.d/pharoscontrol list
+dpkg -i PharosControl-2.0.0-1.ub16.noarch.deb
+apt install -yf
+/etc/init.d/pharoscontrol start
+/etc/init.d/pharoscontrol list
 
