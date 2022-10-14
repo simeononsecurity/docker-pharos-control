8e824798e5525b2782baa982a722d454905fa86b
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2022-10-14 18:00:17 -0500

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -1,6 +1,13 @@
+RETVAL=0
+prog="Pharos Control"
+filename="pharoscontrol"
+lockFile=/var/lock/subsys/$filename
+PIDFILE=/var/run/$filename
+INSTALL_FOLDER=/opt/pharoscontrol
 
 dpkg -i PharosControl-2.0.0-1.ub16.noarch.deb
 apt install -yf
-/etc/init.d/pharoscontrol start
-/etc/init.d/pharoscontrol list
+nohup java -cp "$INSTALL_FOLDER/lib/*" com.tplink.hipap.server.backend.PharosControlHeadless > /dev/nyll 2>&1 & echo $! > $PIDFILE &
+java -cp "$INSTALL_FOLDER/lib/*" com.tplink.hipap.server.backend.mina.MinaClient list
+
 
